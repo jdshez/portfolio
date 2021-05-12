9dc4ac3cf3a5bd968e3c592fdefe8553a6c2a02a
jdshez <jd_shez@protonmail.com>
2021-05-12 21:55:57 +0100

    ammend projects

--- index.js
+++ index.js
@@ -3,3 +3,52 @@ window.onbeforeunload = () => {
       form.reset();
     }
   }
+
+  function hover(element) {
+    let hovImg;
+
+    if (element.classList.contains('row1')) {
+      hovImg = '/imgs/vpnflixhover-min.gif';
+    }
+    else if (element.classList.contains('row2')) {
+      hovImg = '/imgs/firewallhover-min.gif';
+    }
+    if (element.classList.contains('row3')) {
+      hovImg = '/imgs/hotp-min.gif';
+    }
+    if (element.classList.contains('row4')) {
+      hovImg = '/imgs/travel-email-min.gif';
+    }
+    element.querySelector('.row-img').setAttribute('src', hovImg);
+}
+/*function unhover(element) {
+  if (element.classList.contains('row1')) {
+    element.querySelector('img').setAttribute('src', '/imgs/vpnflix-still.png');
+  }
+  else if (element.classList.contains('row2')) {
+    element.querySelector('.row-img').setAttribute('src', '/imgs/firewall.png');
+  }
+  else if (element.classList.contains('row3')) {
+    element.querySelector('img').setAttribute('src', '/imgs/hotp-still.png');
+  }
+  else if (element.classList.contains('row4')) {
+    element.querySelector('.row-img').setAttribute('src', '/imgs/travel-still.png');
+  }
+}*/
+
+function unhover(element) {
+  let unhovImg;
+  if (element.classList.contains('row1')) {
+    unhovImg='/imgs/vpnflix-still.png';
+  }
+  else if (element.classList.contains('row2')) {
+    unhovImg='/imgs/firewall.png';
+  }
+  else if (element.classList.contains('row3')) {
+    unhovImg= '/imgs/hotp-still.png';
+  }
+  else if (element.classList.contains('row4')) {
+    unhovImg= '/imgs/travel-still.png';
+  }
+  element.querySelector('.row-img').setAttribute('src', unhovImg);
+}
